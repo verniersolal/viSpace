8644ecc723e3d8140505fc4a6403c81c02c298cc
verniersolal <verniersolal@gmail.com>
2018-09-28 16:08:19 +0200

    add convert root

--- convert.py
+++ convert.py
@@ -1,19 +1,22 @@
+from flask import Flask, render_template, url_for, request, session, redirect
+from viSpace import app
+
 import json
 
 
 def converter(file, myjson):
     data = []
-    with open(file, "r") as f:
+    with open("data/" + file, "r") as f:
 
         json_tab = []
-        for i in range(3): # On saute les premieres colonnes
+        for i in range(3):  # On saute les premieres colonnes
             next(f)
-        for i,line in enumerate(f):
+        for i, line in enumerate(f):
             if i == 0:
                 words = line.split()
                 for j in words:
                     data.append(j)
-            elif i >=3:
+            elif i >= 3:
                 firsty = line.split()
                 json_tab.append(dict(zip(data, firsty)))
 
@@ -21,3 +24,9 @@ def converter(file, myjson):
 
     jsonfile.write(json.dumps(json_tab))
     jsonfile.close()
+
+
+@app.route('/convertFile', methods=['POST'])
+def convertFile():
+    converter(request.form['filename'],'data.json')
+    return render_template('index.html',converted = True, filename = request.form['filename'])
