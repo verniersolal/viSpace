5d92f225482bbcf498dd1c6dc158d34de568274a
verniersolal <verniersolal@gmail.com>
2018-09-28 16:11:39 +0200

    import index.py

--- viSpace.py
+++ viSpace.py
@@ -3,10 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+from index import *
 
 if __name__ == '__main__':
     app.run()
