c04c2497e88a308200eab0cff86acdaed7249c51
mourad <mourad.inan@hotmail.fr>
2018-11-22 19:11:28 +0300

    Import plusieurs fichiers gz

--- app/controllers/convert.py
+++ app/controllers/convert.py
@@ -1,67 +1,51 @@
 from flask import render_template, url_for, request, redirect
 from app.run import app, mongo
-import os
-from werkzeug.utils import secure_filename
-
 collection = mongo.db.file
+import gzip
 UPLOAD_FOLDER = 'data/'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/upload_file', methods=['GET', 'POST'])
+@app.route('/upload_files', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            print('No file part')
-            return redirect(request.url)
-
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            print('No selected file')
-            return redirect(request.url)
-
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-        return redirect(url_for('convert_file', filename=filename))
+        jsontab = []
+        files = request.files.getlist("file[]")
+        for file in files:
+            jsontab.append(convert_file_to_json(file.filename))
+        return render_template('index.html', converted=True, json_tab=jsontab)
     return render_template('index.html')
-
-
-# This route is called after the importation of the file
-@app.route('/convert-file/<filename>')
-def convert_file(filename):
-    json_tab = convert_file_to_json(filename)
-
-    return render_template('index.html', converted=True, filename=filename, json_tab=json_tab)
 
 
 # This function convert the imported file to json
 # The file have a header which we skipped for the conversion
 def convert_file_to_json(file):
     parameters = []
-    with open(UPLOAD_FOLDER + file, "r") as f:
-
-        json_tab = []
-        for i in range(3):  # we skip the header of the imported file
-            next(f)
-        for i, line in enumerate(f):
-            if i == 0:
-                file_parameters = line.split()
-                for j in file_parameters:
-                    parameters.append(j)
-            elif i >= 4:
-                file_data = line.split()
-                json_tab.append(dict(zip(parameters, file_data)))
-        create_json_file(file, json_tab)
-    return json_tab
+    split = file.split('.')
+    if split[-1] == 'gz':
+        with gzip.open(UPLOAD_FOLDER + file, 'rt') as f:
+            json_tab = []
+            for i in range(3):  # we skip the header of the imported file
+                next(f)
+            for i, line in enumerate(f):
+                if i == 0:
+                    file_parameters = line.split()
+                    for j in file_parameters:
+                        parameters.append(j)
+                elif i >= 4:
+                    file_data = line.split()
+                    json_tab.append(dict(zip(parameters, file_data)))
+        postVisu(file, json_tab)
+        return json_tab
 
 
 # This function create a json file which it contains the json of the imported file
-def create_json_file(json_file_name, json_tab):
-    file_name = json_file_name.split(".")[-1]
-    collection.insert_one({"filename" : file_name, "params": json_tab})
+def postVisu(json_file_name, json_tab):
+    file_name = json_file_name.split(".")[-3] + '.' + json_file_name.split(".")[-2]
+    star_status = json_file_name.split(".")[:3]
+    status = star_status[0] + '.' + star_status[1] + '.' + star_status[2]
+    data = {"filename": file_name, "params": json_tab}
+    print(status)
+    print(data['filename'])
+    collection.insert({status: data}, check_keys=False)
